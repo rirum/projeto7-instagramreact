9a1864a51ec8db81ed43ac319c7e6418ef235495
camille <c.miyashita@gmail.com>
2022-11-18 21:55:26 -0300

    usuario e sugestoes componetizados

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,84 +1,12 @@
+import Usuario from "./Usuario"
+import Sugestoes from "./Sugestoes"
+
 export default function Sidebar(){
     return (
 <div class="sidebar">
-        <div class="usuario" data-test="user">
-          <img src="assets/img/catanacomics.svg" data-test="profile-image"/>
-          <div class="texto">
-            <strong>catanacomics</strong>
-            <span data-test="name">
-              Catana
-              <ion-icon name="pencil" data-test="edit-name"></ion-icon>
-            </span>
-          </div>
-        </div>
-
-        <div class="sugestoes">
-          <div class="titulo">
-            Sugestões para você
-            <div>Ver tudo</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/bad.vibes.memes.svg" />
-              <div class="texto">
-                <div class="nome">bad.vibes.memes</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/chibirdart.svg" />
-              <div class="texto">
-                <div class="nome">chibirdart</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/razoesparaacreditar.svg" />
-              <div class="texto">
-                <div class="nome">razoesparaacreditar</div>
-                <div class="razao">Novo no Instagram</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/adorable_animals.svg" />
-              <div class="texto">
-                <div class="nome">adorable_animals</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="assets/img/smallcutecats.svg" />
-              <div class="texto">
-                <div class="nome">smallcutecats</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-        </div>
-
+        <Usuario />
+        <Sugestoes />
+        
         <div class="links">
           Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
           Hashtags • Idioma
---
+++ src/components/Usuario.js
@@ -0,0 +1,15 @@
+export default function Usuario(){
+    return (
+        <div class="usuario" data-test="user">
+          <img src="assets/img/catanacomics.svg" data-test="profile-image"/>
+          <div class="texto">
+            <strong>catanacomics</strong>
+            <span data-test="name">
+              Catana
+              <ion-icon name="pencil" data-test="edit-name"></ion-icon>
+            </span>
+          </div>
+        </div>
+
+    )
+}
