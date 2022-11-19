2665b4df0e133d6929625e4d5498ce604cd65e0b
camille <c.miyashita@gmail.com>
2022-11-19 01:41:59 -0300

    coração ok

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,29 @@
 import React from "react";
 export default function Post(props){
     const [salvar, setSalvar] = React.useState("bookmark-outline");
+    const [like, setLike] = React.useState("heart-outline");
+    const [classe, setClasse] = React.useState("heart-outline");
+    const [curtidas,setCurtidas] = React.useState(props.curtidas);
+
+
     function salvarPost() {
         if (salvar === "bookmark-outline") {setSalvar("bookmark");
     } else {
         setSalvar("bookmark-outline");
     }}
+
+function likePost(){
+    if (like === "heart-outline") {
+        setLike("heart");
+        setClasse("red");
+        setCurtidas(curtidas + 1);
+}else {
+    setLike("heart-outline");
+    setClasse("black");
+    setCurtidas(curtidas - 1);
+}
+}
+
     return (
         <div class="posts">
             <div class="post" data-test="post">
@@ -26,19 +44,19 @@ export default function Post(props){
             <div class="fundo">
               <div class="acoes">
                 <div>
-                  <ion-icon name="heart-outline" data-test="like-post"></ion-icon>
+                  <ion-icon onClick={likePost} name={like} class={classe} data-test="like-post"></ion-icon>
                   <ion-icon name="chatbubble-outline"></ion-icon>
                   <ion-icon name="paper-plane-outline"></ion-icon>
                 </div>
                 <div>
-                  <ion-icon name={salvar} onClick={salvarPost} data-test="save-post"></ion-icon>
+                  <ion-icon  onClick={salvarPost} name={salvar} data-test="save-post"></ion-icon>
                 </div>
               </div>
 
               <div class="curtidas">
                 <img src={props.imagemCurtida} alt={props.nome} />
                 <div class="texto" data-test="likes-number">
-                  Curtido por <strong>{props.usernameCurtida}</strong> e <strong>outras {props.numero} pessoas</strong>
+                  Curtido por <strong>{props.usernameCurtida}</strong> e <strong>outras {props.curtidas} pessoas</strong> 
                 </div>
               </div>
             </div>
